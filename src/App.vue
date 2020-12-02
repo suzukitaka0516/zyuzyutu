a11de72572b72007318af41854e6fbda1157162d
suzukitaka0516 <takanori_0516@i.softbank.jp>
2020-12-02 19:56:18 +0900

    first-comkmit

---
+++ src/App.vue
@@ -0,0 +1,136 @@
+<template>
+  <body>
+    <div id="zyuzyututop" class="">
+      <div>
+        <div id="">
+        
+        </div>
+        <header>
+          <div>
+           <h1><a href=""><img src="" alt=""></a></h1>
+           <nav>
+             <ul>
+               <li class="new">
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li>
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li class="new">
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li >
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li>
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li>
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li>
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li class="new">
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li>
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li class="new">
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li class="new">
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li class="new">
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+                <li class="newTw">
+                 <a href="">
+                   <span></span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+           <div class="">
+             <h2 class="">SHARE</h2>
+             <ul>
+               <li class="tw"></li>
+               <li class="fb"></li>
+               <li class="ln"></li>
+             </ul>
+           </div>
+          </div>
+        </header>
+        <main>
+          <p class="">
+            <span>
+            </span>
+          </p>
+          <div>
+            <div>
+              <a href="">
+                <span>"PLAY</span>
+                </a>
+            </div>
+          </div>
+          <div>
+            <a href="">
+              <img src="" alt="">
+            </a>
+          </div>
+          <div>
+            <img src="" alt="">
+          </div>
+        </main>
+        <footer>
+          <div class="">
+            <div id=""></div>
+            <small></small>
+          </div>
+        </footer>
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  </body>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style>
+
+</style>
